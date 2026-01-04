23828142f164775071a13cbbeda33df4421a44a3
porosting <porost009@gmail.com>
2026-01-04 12:58:58 +0900

    cofirm move

--- src/drivers/Button.h
+++ src/drivers/Button.h
@@ -24,35 +24,6 @@ class Button {
     // 立ち下がりエッジ（押下）の時のみ true を返す
     bool isPressed();
 
-    bool isHeld();
-        // スイッチの値が変更された場合、デバウンスタイマーをリセット
-        if (reading != lastReading) {
-            lastDebounceTime = millis();
-        }
-
-        if ((millis() - lastDebounceTime) > debounceDelay) {
-            // 読み取り値が何であれ、デバウンス遅延よりも長くその状態にあるため、
-            // それを実際の現在の状態として採用する:
-
-            // ボタンの状態が変わった場合:
-            if (reading != state) {
-                state = reading;
-
-                // 新しい状態が LOW (押下) の場合のみトグルする
-                if (state == LOW) {
-                    pressed = true;
-                }
-            }
-        }
-
-        lastReading = reading;
-        return pressed;
-    }
-
     //押しっぱなしの状態で十分反応する
-    bool isHeld() {
-        // Serial.print("stateは");
-        // Serial.println(state);
-        return (state == LOW);
-    }
+    bool isHeld();
 };
--- src/system/InputManager.h
+++ src/system/InputManager.h
@@ -23,50 +23,5 @@ class InputManager {
     void begin();
 
     //毎フレーム呼ばれてはいる
-    InputEvent update() {
-        // Serial.println(INPUT_BTN_A);
-
-        //この瞬間に、押されたか
-        bool aPressed = btnA.isPressed();
-        bool bPressed = btnB.isPressed();
-
-
-        //同時押し、単押しの順番でやらないと、動きませんよー
-
-        #ifdef DEBUGDAO
-
-        // 同時押しの確認表示用
-        if (aPressed && btnB.isHeld()){
-             Serial.println("同時に押された");
-             return INPUT_BTN_BOTH;
-        }
-
-        if (bPressed && btnA.isHeld()){
-             Serial.println("同時に押された");
-             return INPUT_BTN_BOTH;
-        }
-        #endif
-
-        #ifdef IS_SPRESENSE
-        // 単押しの確認用
-        if (aPressed) {
-            Serial.println("Aだよ");
-            return INPUT_BTN_A;
-        }
-        if (bPressed) {
-            Serial.println("Bだよ");
-            return INPUT_BTN_B;
-        }
-        #endif
-
-        // 同時押しの確認
-        if (aPressed && btnB.isHeld()) return INPUT_BTN_BOTH;
-        if (bPressed && btnA.isHeld()) return INPUT_BTN_BOTH;
-
-        // 単押しの確認
-        if (aPressed) return INPUT_BTN_A;
-        if (bPressed) return INPUT_BTN_B;
-
-        return INPUT_NONE;
-    }
+    InputEvent update();
 };
