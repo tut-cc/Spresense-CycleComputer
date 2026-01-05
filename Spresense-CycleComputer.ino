d525219e7d6743883408bce0992c9196bd973799
rsny <rsny430@gmail.com>
2026-01-05 14:04:20 +0900

    fix: rewrite macro

--- Spresense-CycleComputer.ino
+++ Spresense-CycleComputer.ino
@@ -10,16 +10,10 @@
 #include <LowPower.h>
 #endif
 
-#include "src/drivers/I2CLCDDriver.h"
-#include "src/drivers/LCDDriver.h"
 #include "src/drivers/SevenSegDriver.h"
 #include "src/system/CycleComputer.h"
 
-#if DISPLAY_TYPE == DISPLAY_LCD
-LCDDriver display;
-#elif DISPLAY_TYPE == DISPLAY_I2C_LCD
-I2CLCDDriver display;
-#elif DISPLAY_TYPE == DISPLAY_SEVENSEG
+#if DISPLAY_TYPE == DISPLAY_SEVENSEG
 SevenSegDriver display;
 #endif
 
